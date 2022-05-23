af004963af3762b571224482aa80c6fe77bc2c35
tnshaikh00 <103294764+tnshaikh00@users.noreply.github.com>
2022-05-22 22:22:21 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -33,4 +33,55 @@ public class MarkdownParseTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void snippet1() throws IOException{
+        Path file = Path.of("/Users/MasterKai/Documents/GitHub/Markdown-Parser/Snippet1.md");
+        String content = Files.readString(file);
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<>();
+
+        expected.add("url.com");
+        expected.add("`google.com");
+        expected.add("google.com"); 
+        expected.add("ucsd.edu");
+        
+        for(int i = 0; i < 4; i++){
+            assertEquals(expected.get(i),actual.get(i));
+        }
+    }
+    
+    @Test
+    public void snippet2() throws IOException{
+        Path file = Path.of("/Users/MasterKai/Documents/GitHub/Markdown-Parser/Snippet2.md");
+        String content = Files.readString(file);
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<>();
+
+        expected.add("a.com");
+        expected.add("a.com(())");
+        expected.add("example.com"); 
+        
+        for(int i = 0; i < 3; i++){
+            assertEquals(expected.get(i),actual.get(i));
+        }
+    }
+
+    @Test
+    public void snippet3() throws IOException{
+        Path file = Path.of("/Users/MasterKai/Documents/GitHub/Markdown-Parser/Snippet3.md");
+        String content = Files.readString(file);
+        ArrayList<String> actual = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<>();
+
+        expected.add("https://www.twitter.com");
+        expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule)");
+        expected.add("[this link doesn't have a closing parenthesis](github.com" + 
+                "And there's still some more text after that."); 
+        expected.add("https://cse.ucsd.edu/");
+        
+        for(int i = 0; i < 4; i++){
+            assertEquals(expected.get(i),actual.get(i));
+        }
+    }
 }
